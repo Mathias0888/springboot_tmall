456d4738da25b43825e864a07fdc5bc4c77d8556
liudi <liudi1120083004@icloud.com>
2022-10-09 14:36:04 +0800

    新增前台-我的订单页

--- src/main/java/com/how2java/tmall/service/OrderService.java
+++ src/main/java/com/how2java/tmall/service/OrderService.java
@@ -3,6 +3,7 @@ package com.how2java.tmall.service;
 import com.how2java.tmall.dao.OrderDAO;
 import com.how2java.tmall.pojo.Order;
 import com.how2java.tmall.pojo.OrderItem;
+import com.how2java.tmall.pojo.User;
 import com.how2java.tmall.util.Page4Navigator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -74,5 +75,15 @@ public class OrderService {
 
     public void add(Order order){
         orderDAO.save(order);
+    }
+
+    public List<Order> listByUserWithoutDelete(User user) {
+        List<Order> orders = listByUserAndNotDeleted(user);
+        orderItemService.fill(orders);
+        return orders;
+    }
+
+    public List<Order> listByUserAndNotDeleted(User user) {
+        return orderDAO.findByUserAndStatusNotOrderByIdDesc(user, OrderService.delete);
     }
 }
