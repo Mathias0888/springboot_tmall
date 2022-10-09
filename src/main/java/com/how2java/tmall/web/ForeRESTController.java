aabb7d9ffb4f8a0e7d999576ecef0193455dd12b
liudi <liudi1120083004@icloud.com>
2022-10-09 10:27:50 +0800

    新增前台-我的订单页

--- src/main/java/com/how2java/tmall/web/ForeRESTController.java
+++ src/main/java/com/how2java/tmall/web/ForeRESTController.java
@@ -263,6 +263,17 @@ public class ForeRESTController {
         return order;
     }
 
+    @GetMapping("/forebought")
+    public Object bought(HttpSession session){
+        User user = (User) session.getAttribute("user");
+        if(null==user){
+            return Result.fail("未登录");
+        }
+        List<Order> os = orderService.listByUserWithoutDelete(user);
+        orderService.removeOrderFromOrderItem(os);
+        return os;
+    }
+
 
 
 
