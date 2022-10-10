c329d5c5cbcfd9d508e00c1cddd4cff65230ee4c
liudi <liudi1120083004@icloud.com>
2022-10-10 17:30:21 +0800

    新增Redis(处理到OrderItemService）

--- src/main/java/com/how2java/tmall/service/CategoryService.java
+++ src/main/java/com/how2java/tmall/service/CategoryService.java
@@ -5,6 +5,9 @@ import java.util.List;
 import com.how2java.tmall.pojo.Product;
 import com.how2java.tmall.util.Page4Navigator;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheConfig;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
@@ -15,10 +18,11 @@ import com.how2java.tmall.dao.CategoryDAO;
 import com.how2java.tmall.pojo.Category;
 
 @Service
+@CacheConfig(cacheNames = "categories")
 public class CategoryService {
 	@Autowired CategoryDAO categoryDAO;
 
-
+	@Cacheable(key="'categories-page-'+#p0+ '-' + #p1")
 	public Page4Navigator<Category> list(int start, int size, int navigatePages) {
 		Sort sort = new Sort(Sort.Direction.DESC, "id");
 		Pageable pageable = new PageRequest(start, size,sort);
@@ -26,23 +30,25 @@ public class CategoryService {
 
 		return new Page4Navigator<>(pageFromJPA,navigatePages);
 	}
+	@Cacheable(key="'categories-all'")
 	public List<Category> list() {
     	Sort sort = new Sort(Sort.Direction.DESC, "id");
 		return categoryDAO.findAll(sort);
 	}
-
+	@CacheEvict(allEntries = true)
 	public void add(Category bean) {
 		categoryDAO.save(bean);
 	}
-
+	@CacheEvict(allEntries = true)
 	public void delete(int id) {
 		categoryDAO.delete(id);
 	}
-
+	@Cacheable(key = "'category-one-'+ #p0")
 	public Category get(int id) {
 		Category c= categoryDAO.findOne(id);
 		return c;
 	}
+	@CacheEvict(allEntries = true)
 	public void update(Category bean) {
 		categoryDAO.save(bean);
 	}
