888ffcb66def9ef9aef5252e3699def678273757
liudi <liudi1120083004@icloud.com>
2022-10-11 10:19:13 +0800

    新增Redis(完整版)

--- src/main/java/com/how2java/tmall/service/ProductImageService.java
+++ src/main/java/com/how2java/tmall/service/ProductImageService.java
@@ -4,12 +4,17 @@ import com.how2java.tmall.dao.ProductImageDAO;
 import com.how2java.tmall.pojo.OrderItem;
 import com.how2java.tmall.pojo.Product;
 import com.how2java.tmall.pojo.ProductImage;
+import com.how2java.tmall.util.SpringContextUtil;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheConfig;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
+@CacheConfig(cacheNames = "productImages")
 public class ProductImageService {
 
     public static final String type_single = "single";
@@ -19,30 +24,31 @@ public class ProductImageService {
     ProductImageDAO productImageDAO;
     @Autowired
     ProductService productService;
-
+    @CacheEvict(allEntries = true)
     public void add(ProductImage bean){
         productImageDAO.save(bean);
     }
-
+    @CacheEvict(allEntries = true)
     public void delete(int id){
         productImageDAO.delete(id);
     }
-
+    @Cacheable(key = "'productImages-one-'+ #p0")
     public ProductImage get(int id){
         ProductImage productImage = productImageDAO.findOne(id);
         return productImage;
     }
-
+    @Cacheable(key = "'productImages-single-pid-'+ #p0.id")
     public List<ProductImage> listSingleProductImages(Product product){
         return productImageDAO.findByProductAndTypeOrderByIdDesc(product,type_single);
     }
-
+    @Cacheable(key = "'productImages-detail-pid-'+ #p0.id")
     public List<ProductImage> listDetailProductImages(Product product){
         return productImageDAO.findByProductAndTypeOrderByIdDesc(product,type_detail);
     }
 
     public void setFirstProductImage(Product product){
-        List<ProductImage> singleImages = listSingleProductImages(product);
+        ProductImageService productImageService = SpringContextUtil.getBean(ProductImageService.class);
+        List<ProductImage> singleImages = productImageService.listSingleProductImages(product);
         if(!singleImages.isEmpty())
             product.setFirstProductImage(singleImages.get(0));
         else
